44031cee50b5b87b856f62a8d52a1f7360745f2d
Paul Schifferer <paul@schifferers.net>
2017-06-07 15:54:33 -0700

    Implement the user model.

--- Sources/MediumKit/Models/User.swift
+++ Sources/MediumKit/Models/User.swift
@@ -4,22 +4,43 @@ import Foundation
 private let dateFormatter = ISO8601DateFormatter()
 
 public struct User {
-
+    public let id : String
+    public let username : String
+    public let name : String
+    public let url : URL
+    public let imageURL : URL
 }
 
 
 extension User : Serializable {
 
-    public init?(from dict : JSONDictionary) {
+    public init?(from json : JSONDictionary) {
+        guard let id = json["id"] as? String,
+            let username = json["username"] as? String,
+            let name = json["name"] as? String,
+            let u = json["url"] as? String,
+            let url = URL(string: u),
+            let i = json["imageUrl"] as? String,
+            let imageUrl = URL(string: i)
+            else { return nil }
 
+        self.id = id
+        self.username = username
+        self.name = name
+        self.url = url
+        self.imageURL = imageUrl
     }
 
     public func toDictionary() -> JSONDictionary {
-        var dict : JSONDictionary = [
-
+        let dict : JSONDictionary = [
+            "id" : id,
+            "username" : username,
+            "name" : name,
+            "url" : url.absoluteString,
+            "imageUrl" : imageURL.absoluteString,
             ]
-
-               return dict
+        
+        return dict
     }
     
 }
